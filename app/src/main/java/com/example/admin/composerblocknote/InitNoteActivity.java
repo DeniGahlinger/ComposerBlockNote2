e87ad073170fb2dc5b262e7d16e405d79df84897
Guillaume Noguera <guillaume.noguera@gmail.com>
2017-11-10 10:45:01 +0100

    Added directory creation

--- app/src/main/java/com/example/admin/composerblocknote/InitNoteActivity.java
+++ app/src/main/java/com/example/admin/composerblocknote/InitNoteActivity.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.data;
 
 public class InitNoteActivity extends AppCompatActivity {
 
     final List<String> tempoList = new ArrayList<String>();
     Spinner spTemp;
     Spinner spSign;
+    EditText textSongName;
+    EditText textPartName;
+
     ArrayAdapter<String> adp1;
     boolean isNewSong;
 
@@ -27,9 +35,13 @@ public class InitNoteActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String str = intent.getStringExtra("isNewSong");
-        isNewSong = true;
+        isNewSong = intent.getStringExtra("isNewSong").equals("yes");
+
         spTemp = (Spinner) findViewById(R.id.spnTempo);
         spSign = (Spinner) findViewById(R.id.spnSignature);
+        textSongName = (EditText) findViewById((R.id.tbxSongName));
+        textPartName = (EditText) findViewById(R.id.tbxPartName);
+
         final Spinner spSign2 = (Spinner) findViewById(R.id.spnSign1);
         initLists();
         adp1 = new ArrayAdapter<String>(
@@ -39,6 +51,7 @@ public class InitNoteActivity extends AppCompatActivity {
 
         adp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spTemp.setAdapter(adp1);
+
         if(str.equals("yes")){
             spTemp.setSelection(100);
             spSign.setSelection(2);
@@ -54,9 +67,38 @@ public class InitNoteActivity extends AppCompatActivity {
             findViewById(R.id.tbxSongName).setVisibility(View.INVISIBLE);
         }
         findViewById(R.id.btnOK).setOnClickListener(new View.OnClickListener() {
+            //creating folder and writing metadata
             @Override
             public void onClick(View v) {
-                
+                String dirName = getIntent().getStringExtra("mainDirName");
+                File baseFolder = (File)getIntent().getExtras().get("mainDir");
+                if (isNewSong){
+                    File songFolder = new File(baseFolder.getAbsolutePath() + "/" + textSongName.getText());
+                    File partFolder = new File(songFolder.getAbsolutePath() + "/" + textPartName.getText());
+
+                    if (songFolder.mkdir()){
+                        partFolder.mkdir();
+                        File metadata = new File(songFolder.getAbsolutePath() + "/.meta");
+                        //temp
+                        metadata.delete();
+                        String content = spTemp.getSelectedItem().toString() + ";" + spSign.getSelectedItem().toString() + ";" + spSign2.getSelectedItem().toString();
+                        System.out.println(content);
+                        try{
+                            metadata.createNewFile();
+                            metadata.setWritable(true);
+                            FileOutputStream fos = new FileOutputStream(metadata);
+                            fos.write(content.getBytes());
+                            fos.flush();
+                            fos.close();
+                        }
+                        catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    else{
+                        Log.d("NRV", "Wasn't able to create folder .. ");
+                    }
+                }
             }
         });
     }
